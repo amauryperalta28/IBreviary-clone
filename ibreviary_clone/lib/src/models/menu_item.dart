d1e063b9fd2bc820e3a3962b1b6cfd6de2bda20b
amauryperalta28 <amauryperalta28@gmail.com>
2020-05-12 18:43:34 -0400

    -Added menu with subtitles

--- ibreviary_clone/lib/src/models/menu_item.dart
+++ ibreviary_clone/lib/src/models/menu_item.dart
@@ -1,9 +1,11 @@
 class MenuItem {
   String key;
   String description;
+  String subtitle = '';
 
-  MenuItem({String key, String description}){
+  MenuItem({String key, String description, String subtitle}){
      this.key = key;
      this.description = description;
+     this.subtitle = subtitle;
   }
 }
--- ibreviary_clone/lib/src/pages/prayers_page.dart
+++ ibreviary_clone/lib/src/pages/prayers_page.dart
@@ -5,7 +5,7 @@ import 'ibreviary_app_bar.dart';
 
 class PrayersPage extends StatelessWidget {
   final textStyle = TextStyle(fontSize: 20.0);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,38 +17,95 @@ class PrayersPage extends StatelessWidget {
     );
   }
 
-  Widget _buildBody() {
+  List<MenuItem> _getMenuItems() {
     List<MenuItem> options = [
-      MenuItem(description:'Ave Maria', key: 'AveMaria'),
-      MenuItem(description:'Misterios del Rosario', key: 'MisteriosRosario'),
-      MenuItem(description:'El Santo Rosario', key: 'ElSantoRosario'),
-      MenuItem(description:'Letanias Lauretanas', key: 'LetaniasLauretanas'),
-      MenuItem(description:'Magnificat', key: 'Magnificat'),
-      MenuItem(description:'Salve', key: 'Salve'),
-      MenuItem(description:'El Angelus', key: 'El Angelus'),
-      MenuItem(description:'Reina del Cielo', key: 'ReinaCielo'),
-      MenuItem(description:'Bendita sea tu pureza', key: 'BenditaPureza'),
-      MenuItem(description:'Sub tuum praesidium. Bajo tu amparo nos acogemos', key: 'Amparo'),
-      MenuItem(description:'Simbolo de los Apostoles', key: 'SimboloApostoles'),
-      MenuItem(description:'Te Deum', key: 'TeDeum'),
-      MenuItem(description:'Himno <<Veni Creator>>', key: 'VeniCreator'),
-      MenuItem(description:'Secuencia del Pentecostes', key: 'SecuenciaPentecostes'),
-      MenuItem(description:'Padre Nuestro', key: 'PadreNuestro'),
-      MenuItem(description:'Alma de Cristo', key: 'AlmaCristo'),
-      MenuItem(description:'Lauda Sion(latino)', key: 'Lauda'),
-      MenuItem(description:'Gloria', key: 'Gloria'),
-      MenuItem(description:'El credo Niceno', key: 'CredoNiceno'),
-      MenuItem(description:'Adoracion Eucaristica', key: 'AdoracionEucaristica')
-      
+      MenuItem(description: 'Ave Maria', key: 'AveMaria'),
+      MenuItem(description: 'Misterios del Rosario', key: 'MisteriosRosario'),
+      MenuItem(description: 'El Santo Rosario', key: 'ElSantoRosario'),
+      MenuItem(description: 'Letanias Lauretanas', key: 'LetaniasLauretanas'),
+      MenuItem(description: 'Magnificat', key: 'Magnificat'),
+      MenuItem(description: 'Salve', key: 'Salve'),
+      MenuItem(description: 'El Angelus', key: 'El Angelus'),
+      MenuItem(description: 'Reina del Cielo', key: 'ReinaCielo'),
+      MenuItem(description: 'Bendita sea tu pureza', key: 'BenditaPureza'),
+      MenuItem(
+          description: 'Sub tuum praesidium. Bajo tu amparo nos acogemos',
+          key: 'Amparo'),
+      MenuItem(
+          description: 'Simbolo de los Apostoles', key: 'SimboloApostoles'),
+      MenuItem(description: 'Te Deum', key: 'TeDeum'),
+      MenuItem(description: 'Himno <<Veni Creator>>', key: 'VeniCreator'),
+      MenuItem(
+          description: 'Secuencia del Pentecostes',
+          key: 'SecuenciaPentecostes'),
+      MenuItem(description: 'Padre Nuestro', key: 'PadreNuestro'),
+      MenuItem(description: 'Alma de Cristo', key: 'AlmaCristo'),
+      MenuItem(description: 'Lauda Sion(latino)', key: 'Lauda'),
+      MenuItem(description: 'Gloria', key: 'Gloria'),
+      MenuItem(description: 'El credo Niceno', key: 'CredoNiceno'),
+      MenuItem(
+          description: 'Adoracion Eucaristica', key: 'AdoracionEucaristica'),
+      MenuItem(
+          description: 'Suplica a la Reina del santo Rosario de Pompeya',
+          key: 'Pompeya')
     ];
 
-    var result = options
+    return options;
+  }
+
+  List<MenuItem> _getMenuItemsWithSubtitles() {
+    List<MenuItem> optionsWithSubtitles = [
+      MenuItem(
+          description: 'San Luigi Guanella',
+          key: 'Guanella',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description: 'Oracion a San Jose',
+          key: 'Jose',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description: 'Rosario de la Providencia',
+          key: 'Providencia',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description:
+              'Oracion por la intercesion de la Bella Sor Clara Bosatta',
+          key: 'Bosatta',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description:
+              'Oracion de la intercesion al siervo de Dios Mons. Aurelio Bancciarini',
+          key: 'Bancciarini',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description: 'Oracion de la intercesion de San Luis Guanella',
+          key: 'SanLuisGuanella',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description: 'Oracion por las vocaciones',
+          key: 'vocaciones',
+          subtitle: 'Siervos de la Caridad(Guanelianos)'),
+      MenuItem(
+          description: 'Oracion a Maria por el COVID19 - Papa Francisco',
+          key: 'COVID19'),
+      MenuItem(
+          description:
+              'Oracion a Maria Salus Popolo Romani por el COVID19 - Papa Francisco',
+          key: 'PopoloRomani')
+    ];
+
+    return optionsWithSubtitles;
+  }
+
+  Widget _buildBody() {
+    var result = _getMenuItems()
         .map((menuItem) => Column(
               children: <Widget>[
                 Container(
                   height: 50,
                   color: Colors.orange[200],
-                  child: Center(child: Text(menuItem.description, style: textStyle)),
+                  child: Center(
+                      child: Text(menuItem.description, style: textStyle)),
                 ),
                 Divider(
                   thickness: 2.0,
@@ -56,6 +113,24 @@ class PrayersPage extends StatelessWidget {
               ],
             ))
         .toList();
+
+    var resultWithSubtitles = _getMenuItemsWithSubtitles()
+        .map((menuItem) => Column(
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  color: Colors.orange[200],
+                  child: Center(
+                      child: Text(menuItem.description, style: textStyle)),
+                ),
+                Divider(
+                  thickness: 2.0,
+                )
+              ],
+            ))
+        .toList();
+
+    result.addAll(resultWithSubtitles);
 
     return ListView(
       padding: const EdgeInsets.all(8),
