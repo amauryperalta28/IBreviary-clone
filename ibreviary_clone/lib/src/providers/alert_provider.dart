af112a9b923969957eee995f98c9f6cc572cd22f
amauryperalta28 <amauryperalta28@gmail.com>
2020-05-12 19:37:29 -0400

    Removed modal conten bottom padding

--- ibreviary_clone/lib/src/providers/alert_provider.dart
+++ ibreviary_clone/lib/src/providers/alert_provider.dart
@@ -5,32 +5,42 @@ class _AlertProvider {
     showDialog(
         context: context,
         barrierDismissible: true,
+        
         builder: (context) {
           return AlertDialog(
             title: Text(text, style: TextStyle(fontSize: 18)),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text('Cancelar',
-                          style: TextStyle(color: Colors.red, fontSize: 18)),
-                    ),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text('OK',
-                          style: TextStyle(color: Colors.red, fontSize: 18)),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            content: _buildModalContent(context),
+            contentPadding: EdgeInsets.only(bottom: 0.0),
           );
         });
+  }
+
+  Widget _buildModalContent(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar',
+                  style: TextStyle(color: Colors.red, fontSize: 18)),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child:
+                  Text('OK', style: TextStyle(color: Colors.red, fontSize: 18)),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
 
